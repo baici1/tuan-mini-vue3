4c07fa23f226e1e8a92b926735579e58b4fa2174
baici1 <249337001@qq.com>
2023-01-01 12:51:09 +0800

    :sparkles: 实现effect readonly功能：只读&重构化reactive.ts代码

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,52 @@
+import { track, trigger } from './effect';
+/**
+ * @description: 无需重复执行createGetter(),createSetter()等函数，第一次执行进行缓存
+ */
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+/**
+ * @description:在reactive，readon 等函数中发现 get部分 存在重复代码，进行重构成createGetter
+ * tip 对照reactive，readon 中的 get 代码前后对照，了解重构必要性
+ * @param {*} isReadonly 只读的标志
+ * @return {*}
+ */
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    if (!isReadonly) {
+      // 依赖收集
+      track(target, key);
+    }
+    return res;
+  };
+}
+/**
+ * @description:在reactive，readon 等函数中 set部分 发现存在重复代码，进行重构成createGetter
+ * @return {*} set function
+ */
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    // 触发依赖
+    trigger(target, key);
+    return res;
+  };
+}
+/**
+ * @description: 用于重构reactive等函数Proxy重复结构，简化内部函数
+ */
+export const mutableHandlers = {
+  get,
+  set,
+};
+/**
+ * @description: 用于重构readonly等函数Proxy重复结构，简化内部函数
+ */
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(`key:${key} set failed because target is readonly`, target);
+    return true;
+  },
+};
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,17 +1,21 @@
+import { mutableHandlers, readonlyHandlers } from './baseHandlers';
 import { track, trigger } from './effect';
+
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      // 依赖收集
-      track(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      // 触发依赖
-      trigger(target, key);
-      return res;
-    },
-  });
+  return createActionObject(raw, mutableHandlers);
+}
+/**
+ * @description: 只读：是不需要做依赖收集，也不会去触发依赖
+ * @param {*} raw
+ */
+export function readonly(raw) {
+  return createActionObject(raw, readonlyHandlers);
+}
+/**
+ * @description: 增加可读性，抽离重复代码
+ * @param {any} raw
+ * @param {*} baseHandlers
+ */
+function createActionObject(raw: any, baseHandlers) {
+  return new Proxy(raw, baseHandlers);
 }
